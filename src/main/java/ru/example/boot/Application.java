5f5f9118edc4151fb7d9e1143579e6ccce287ac4
homeuser <zowie_i@mail.ru>
2020-08-07 15:57:34 +0300

    03.Manual Configuration - Java configuration

--- src/main/java/ru/example/boot/Application.java
+++ src/main/java/ru/example/boot/Application.java
@@ -1,13 +1,16 @@
 package ru.example.boot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
+@Import(MyConfiguration.class)
 public class Application {
     @Value("${name}")
     String name;
@@ -19,5 +22,13 @@ public class Application {
     @RequestMapping("/")
     public String hello() {
         return "Hello from " + name + "!";
+    }
+
+    @Autowired
+    String message;
+
+    @RequestMapping("/message")
+    public String helloBean() {
+        return message;
     }
 }
---
+++ src/main/java/ru/example/boot/MyConfiguration.java
@@ -0,0 +1,12 @@
+package ru.example.boot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfiguration {
+    @Bean
+    public String message(){
+        return "Hello from Bean";
+    }
+}
