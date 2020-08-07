1df1173dbb3690fe14676928f7eea85520e10cf9
homeuser <zowie_i@mail.ru>
2020-08-07 13:38:28 +0300

    03.Manual Configuration - application.properties

--- src/main/java/ru/example/boot/Application.java
+++ src/main/java/ru/example/boot/Application.java
@@ -1,5 +1,6 @@
 package ru.example.boot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableAutoConfiguration
 public class Application {
+    @Value("${name}")
+    String name;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @RequestMapping("/")
-    public String hello(){
-        return "Hello, World!";
+    public String hello() {
+        return "Hello from " + name + "!";
     }
 }
