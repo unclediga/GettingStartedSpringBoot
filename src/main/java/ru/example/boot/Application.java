7ed4cd447714ca7f54296de74b12194b351a7a03
homeuser <zowie_i@mail.ru>
2020-08-07 16:00:58 +0300

    03.Manual Configuration - Java configuration (ver. with component scan)

--- src/main/java/ru/example/boot/Application.java
+++ src/main/java/ru/example/boot/Application.java
@@ -4,13 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
-@Import(MyConfiguration.class)
+@ComponentScan
 public class Application {
     @Value("${name}")
     String name;
